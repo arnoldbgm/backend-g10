c60bf5c0cfa030790735e682921e6dca8b38274e
arnoldbgm <arnold.gallegosm@gmail.com>
2023-01-03 21:14:17 -0500

    changes

--- Dia-2/inicios_sql.sql
+++ Dia-2/inicios_sql.sql
@@ -19,3 +19,38 @@ CREATE TABLE usuarios(
     dni CHAR(8) UNIQUE
 );
 
+-- cuando no entra, nos vamos a mysql80 y le damos en iniciar
+
+create table tareas(
+	id 			INT 	auto_increment primary key,
+    titulo  	varchar(100) 	unique,
+    descripcion text,
+    usuario_id INT,
+    -- Creo la relacion entre las tablas
+    -- en foreing digo que prop de mi tabla va tener el foreign key
+    -- el reference señala que columna de la tb usuarios me voy a jalar
+    foreign key (usuario_id) references usuarios(id)
+);
+
+-- sub lenguajes
+-- DDL (DATA DEFINITION LANGUAGE)
+-- Eliminar tablas bd, crear tb
+-- 	 Create (crear)
+--	ALTER (actualizar)
+--  DROP (ELIMINAR TODO)
+-- DML  (DATA MANIPULATE LANGUAGE)
+-- es un lng que sirve que para definir la infromacion que ira dentro de las estructuras
+-- insertar datos, actualizar datos, eliminar datos y leer datos
+-- INSERT (insertar)
+-- select (visualizar)
+-- upadte (actualizar)
+-- delete (eliminar)
+
+insert into usuarios (nombre, dni) values ('Arnold', '70036504');
+
+insert into usuarios (id, nombre, dni) values(default, 'Juana', '12345678');
+
+-- Si queremos ingresar varios registros
+insert into usuarios (id, nombre, dni) values(default, 'Diego', '12345671'), 
+											(default, 'Pablo', '12345679'),
+											(default, 'Vicente', '12345670');
